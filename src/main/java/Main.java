fa1dd07feaee97b1bc06262086deee418973668e
sol27sanj-hash <sol27sanj@gmail.com>
2026-04-20 14:14:20 +0530

    third usecase

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,13 @@
-import java.util.Random;
+import java.util.Scanner;
 public class Main {
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
     public static void main(String[] args) {
-        tossAndAssignSymbols();
-        displayTossResult();
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
     }
-    static void tossAndAssignSymbols() {
-        Random random = new Random();
-        int toss = random.nextInt(2);
-        if (toss == 0) {
-            isHumanTurn = true;
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-        } else {
-            isHumanTurn = false;
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-    static void displayTossResult() {
-        if (isHumanTurn) {
-            System.out.println("Human won the toss and will play first.");
-        } else {
-            System.out.println("Computer won the toss and will play first.");
-        }
-
-        System.out.println("Human symbol: " + humanSymbol);
-        System.out.println("Computer symbol: " + computerSymbol);
+    static int getUserSlot() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+        return slot;
     }
 }
