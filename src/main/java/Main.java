1b3bbce9395c9ac0db7cb892b89ec19bfa5ec35b
sol27sanj-hash <sol27sanj@gmail.com>
2026-04-21 15:43:36 +0530

    seventh usecase

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,36 @@
-public class Main{
-    static char[][] board = new char[3][3];
+import java.util.Random;
+public class Main {
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+    static char computerSymbol = 'O';
     public static void main(String[] args) {
-        placeMove(0, 0, 'X');
-        System.out.println(board[0][0]); // expected: X
+        computerMove();
+        printBoard();
     }
-    static void placeMove(int row, int col, char symbol) {
-        board[row][col] = symbol;
+    static void computerMove() {
+        Random rand = new Random();
+        int slot;
+        int row, col;
+        while (true) {
+            slot = rand.nextInt(9) + 1;
+            row = (slot - 1) / 3;
+            col = (slot - 1) % 3;
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                System.out.println("Computer chose slot: " + slot);
+                break;
+            }
+        }
+    }
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
