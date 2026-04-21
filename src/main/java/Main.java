a8d01873a6a96cad24a03832984e4cfabf37ce3a
sol27sanj-hash <sol27sanj@gmail.com>
2026-04-21 15:11:58 +0530

    fifth usecase

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,10 @@
-public class Main {
-
-    static char[][] board = {
-            {'-', '-', '-'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
-    };
-
+public class Main{
+    static char[][] board = new char[3][3];
     public static void main(String[] args) {
-        System.out.println(isValidMove(1, 1)); // true
-        System.out.println(isValidMove(3, 0)); // false (out of bounds)
-
-        board[1][1] = 'X';
-        System.out.println(isValidMove(1, 1)); // false (already filled)
+        placeMove(0, 0, 'X');
+        System.out.println(board[0][0]); // expected: X
     }
-
-    // UC5: Validate move
-    static boolean isValidMove(int row, int col) {
-
-        // 1. Check bounds
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-
-        // 2. Check if cell is empty
-        if (board[row][col] != '-') {
-            return false;
-        }
-
-        return true;
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
     }
 }
